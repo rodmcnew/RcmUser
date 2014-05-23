3f02b922792f4209948c6de97dd8740a5c081585
James Jervis <jjervis@relivinc.com>
2014-05-23 12:08:31 -0500

    Added interface check

---
+++ src/RcmUser/Acl/Provider/ResourceProviderInterface.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * ResourceProviderInterface.php
+ *
+ * ResourceProviderInterface
+ *
+ * PHP version 5
+ *
+ * @category  Reliv
+ * @package   RcmUser\Acl\Provider
+ * @author    James Jervis <jjervis@relivinc.com>
+ * @copyright 2014 Reliv International
+ * @license   License.txt New BSD License
+ * @version   GIT: <git_id>
+ * @link      https://github.com/reliv
+ */
+
+namespace RcmUser\Acl\Provider;
+
+use RcmUser\Exception\RcmUserException;
+
+
+/**
+ * Interface ResourceProviderInterface
+ *
+ * ResourceProviderInterface Interface
+ *
+ * PHP version 5
+ *
+ * @category  Reliv
+ * @package   RcmUser\Acl\Provider
+ * @author    James Jervis <jjervis@relivinc.com>
+ * @copyright 2014 Reliv International
+ * @license   License.txt New BSD License
+ * @version   Release: <package_version>
+ * @link      https://github.com/reliv
+ */
+interface ResourceProviderInterface
+{
+    /**
+     * setProviderId
+     *
+     * @param string $providerId providerId
+     *
+     * @return void
+     */
+    public function setProviderId($providerId);
+
+    /**
+     * getProviderId
+     *
+     * @return string
+     */
+    public function getProviderId();
+
+
+    /**
+     * getResources (ALL resources)
+     * Return a multi-dimensional array of resources and privileges
+     * containing ALL possible resources including run-time resources
+     *
+     * @return array
+     */
+    public function getResources();
+
+    /**
+     * getResource
+     * Return the requested resource
+     * Can be used to return resources dynamically at run-time
+     *
+     * @param $resourceId
+     *
+     * @return array
+     */
+    public function getResource($resourceId);
+} 
