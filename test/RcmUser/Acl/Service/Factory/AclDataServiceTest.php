1867d0623e6c4cb5fa1e4affc7d79adf56f99e02
James Jervis <jjervis@relivinc.com>
2014-06-24 17:46:31 -0500

    Unit tests

---
+++ test/RcmUser/Acl/Service/Factory/AclDataServiceTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * AclDataServiceTest.php
+ *
+ * LongDescHere
+ *
+ * PHP version 5
+ *
+ * @category  Reliv
+ * @package   RcmUser\Test\Acl\Service\Factory
+ * @author    James Jervis <jjervis@relivinc.com>
+ * @copyright 2014 Reliv International
+ * @license   License.txt New BSD License
+ * @version   GIT: <git_id>
+ * @link      https://github.com/reliv
+ */
+
+namespace RcmUser\Test\Acl\Service\Factory;
+
+require_once __DIR__ . '/../../../../Zf2TestCase.php';
+
+use RcmUser\Acl\Service\Factory\AclDataService;
+use RcmUser\Zf2TestCase;
+
+/**
+ * Class AclDataServiceTest
+ *
+ * AclDataServiceTest
+ *
+ * PHP version 5
+ *
+ * @category  Reliv
+ * @package   RcmUser\Test\Acl\Service\Factory
+ * @author    James Jervis <jjervis@relivinc.com>
+ * @copyright 2014 Reliv International
+ * @license   License.txt New BSD License
+ * @version   Release: <package_version>
+ * @link      https://github.com/reliv
+ */
+class AclDataServiceTest extends Zf2TestCase
+{
+
+    /**
+     * test
+     *
+     * @return void
+     */
+    public function test()
+    {
+
+        $factory = new AclDataService();
+
+        $service = $factory->createService($this->getMockServiceLocator());
+        $this->assertInstanceOf(
+            'RcmUser\Acl\Service\AclDataService',
+            $service
+        );
+
+        //
+        $this->assertInstanceOf(
+            'RcmUser\Acl\Db\AclRoleDataMapperInterface',
+            $service->getAclRoleDataMapper()
+        );
+
+        $this->assertInstanceOf(
+            'RcmUser\Acl\Db\AclRuleDataMapperInterface',
+            $service->getAclRuleDataMapper()
+        );
+
+    }
+}
+ 
