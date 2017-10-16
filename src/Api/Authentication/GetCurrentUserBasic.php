80ab150e4b4f7020d401dfbbdcab4bd05543f03d
James Jervis <jjervis@relivinc.com>
2017-10-16 13:44:54 -0500

    Fix

--- src/Api/Authentication/GetCurrentUserBasic.php
+++ src/Api/Authentication/GetCurrentUserBasic.php
@@ -5,7 +5,7 @@ namespace RcmUser\Api\Authentication;
 /**
  * @author James Jervis - https://github.com/jerv13
  */
-class GetCurrentUserBasic extends GetIdentityBasic
+class GetCurrentUserBasic extends GetIdentityBasic implements GetCurrentUser
 {
 
 }
